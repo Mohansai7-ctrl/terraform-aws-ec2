722ffeaa5690e991450b5beed42c9235405e6f79
Mohansai <bandarumohansai@gmail.com>
2024-09-19 22:04:19 +0530

    restricting the users to use few options like instance_types

--- variables.tf
+++ variables.tf
@@ -5,11 +5,16 @@ variable "ami_id" {
 
 variable "instance_type_info" {
     default = "t3.micro"
+    validation {
+        condition = contains(["t3.micro","t3.medium","t3.nano"], var.instance_type_info)
+        error_message = "instance_type should only be (t3.micro,t3.medium,t3.nano)"
+    }
 }
 
 variable "security_group_id" {
     type = list(string)
-    #default = ["sg-0d91387712ba38962"] #if its is uncommented(not provided) then it will check in user module(ec2-module-demo)
+    #default = ["sg-0d91387712ba38962"] #if its is commented(not provided) then it will check in user module(ec2-module-demo)
+    
 }
 
 variable "tags_name" {
